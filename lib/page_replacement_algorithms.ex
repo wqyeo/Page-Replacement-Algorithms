0230e941a4f5cf3d448dc866f977e888c5513f10
Yeo Wen Qin, Kaynn <KaynnCahya@outlook.com>
2023-04-04 23:30:06 +0800

    Init

---
+++ lib/page_replacement_algorithms.ex
@@ -0,0 +1,18 @@
+defmodule PageReplacementAlgorithms do
+  @moduledoc """
+  Documentation for `PageReplacementAlgorithms`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PageReplacementAlgorithms.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/page_replacement_algorithms_test.exs
@@ -0,0 +1,8 @@
+defmodule PageReplacementAlgorithmsTest do
+  use ExUnit.Case
+  doctest PageReplacementAlgorithms
+
+  test "greets the world" do
+    assert PageReplacementAlgorithms.hello() == :world
+  end
+end
